19aeb265c381928fc1a1528b1072f69e89dee1fa
eli_0903 <91484965+Walts03@users.noreply.github.com>
2024-04-11 03:43:11 +1000

    change date
    change date

--- arrow.sql
+++ arrow.sql
@@ -1,4 +1,4 @@
-INSERT INTO Arrow (Dist, ShootingRange, Score, ArrowDate) VALUES
+INSERT INTO Arrow (ShootingSessionID, Dist, ShootingRange, Score, ArrowDate) VALUES
 (60, 6, 9, '2024-06-20 10:34:12'),
 (50, 6, 10, '2024-06-20 10:50:27'),
 (40, 6, 8, '2024-06-20 11:15:17'),
@@ -6,5 +6,5 @@ INSERT INTO Arrow (Dist, ShootingRange, Score, ArrowDate) VALUES
 (50, 5, 7, '2024-04-15 12:21:31'),
 (30, 5, 9, '2024-04-15 12:43:21'),
 (30, 5, 4, '2024-04-15 12:59:13'),
-(70, 12, 9, '2024-04-15 13:22:27'),
-(18, 10, 7, '2024-04-15 13:45:32');
+(70, 12, 9, '2024-04-16 13:22:27'),
+(18, 10, 7, '2024-04-17 13:45:32');
--- shootingsession.sql
+++ shootingsession.sql
@@ -1,5 +1,5 @@
 INSERT INTO ShootingSession (RoundName, Division, Class, ShootingDate) VALUES
-('WA900', 'R', 'Male Open', '2024-04-21'),
-('WA70', 'C', 'Male Open', '2024-04-19'),
+('WA900', 'R', 'Male Open', '2024-04-15'),
+('WA70', 'C', 'Male Open', '2024-04-16'),
 ('WA18', 'B', 'Female Open', '2024-04-17'),
-('Metric II', 'L', 'Under 21 Male', '2024-04-15');
+('Metric II', 'L', 'Under 21 Male', '2024-06-20');
