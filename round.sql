18684bbb580c1812ad0a2bccbfa67dee8879e3f2
eli_0903 <91484965+Walts03@users.noreply.github.com>
2024-04-11 04:22:24 +1000

    fixing arrow, round, shootingsess
    fixing

--- round.sql
+++ round.sql
@@ -1,6 +1,6 @@
 INSERT INTO Round (Name, MaxScoreByArrow) VALUES
-('WA900', 900),
-('WA70', 720),
-('WA18', 600),
-('Metric II', 1400);
+('WA900', 10),
+('WA70', 10),
+('WA18', 10),
+('Metric II', 10);
 
--- shootingsession.sql
+++ shootingsession.sql
@@ -1,5 +1,5 @@
-INSERT INTO ShootingSession (RoundName, Division, Class, ShootingDate) VALUES
+INSERT INTO ShootingSession (RoundName, Division, Class, SessionDate) VALUES
+('Metric II', 'L', 'Under 21 Male', '2024-06-20'),
 ('WA900', 'R', 'Male Open', '2024-04-15'),
 ('WA70', 'C', 'Male Open', '2024-04-16'),
-('WA18', 'B', 'Female Open', '2024-04-17'),
-('Metric II', 'L', 'Under 21 Male', '2024-06-20');
+('WA18', 'B', 'Female Open', '2024-04-17');
